d3f4a98f25ba6d0a55db2a8c3af4f1c4a2b9ef9d
Aram32mm <a01657142@tec.mx>
2023-09-25 20:47:45 -0600

    bug de scores arreglado

--- app/core/views.py
+++ app/core/views.py
@@ -3,9 +3,8 @@ Vistas para el CORE.
 """
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema
 
-@extend_schema
+
 @api_view(['GET'])
 def health_check(request):
     """Returns successful response."""
